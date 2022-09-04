2ac19b07b1324dbe1497f826f03a2c1d8938f236
kk <758124607@qq.com>
2022-09-04 12:06:58 +0800

    增加屏幕适配

---
+++ lib/xx_util/screen/screen_util.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
+///必须在首页进行初始化
+initScreenUtil({
+  required double width,
+  required double height,
+  required BuildContext context,
+}) {
+  ScreenUtil.init(
+    //设备像素大小
+    context,
+    //设计尺寸
+    designSize: Size(width, height),
+  );
+}
+
+//字体缩放
+///按照屏幕宽缩放
+double scale(num width) {
+  return ScreenUtil().setWidth(width);
+}
+
+///按照屏幕高缩放
+double scaleByHeight(num height) {
+  return ScreenUtil().setHeight(height);
+}
+
+///字体适配
+double font(double fontSize) {
+  return fontSize.sp;
+}
+
+///屏幕宽度
+double getScreenWidth() {
+  return ScreenUtil().screenWidth;
+}
+
+///屏幕高度
+double getScreenHeight() {
+  return ScreenUtil().screenHeight;
+}
+
+///屏幕密度
+double getScreenPixelRatio() {
+  return ScreenUtil().pixelRatio ?? 1;
+}
+
+///statusBarHeight
+double getStatusBarHeight() {
+  return ScreenUtil().statusBarHeight;
+}
--- lib/xx_util/xx_util.dart
+++ lib/xx_util/xx_util.dart
@@ -1,1 +1,2 @@
 export 'package:xx_vendor/xx_util/color/color_util.dart';
+export 'screen/screen_util.dart';
