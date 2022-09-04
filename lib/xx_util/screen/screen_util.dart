cfd3a8ea309bc406e10dc3008509376eb640b0b4
kk <758124607@qq.com>
2022-09-04 12:08:05 +0800

    增加屏幕适配

--- lib/xx_util/screen/screen_util.dart
+++ lib/xx_util/screen/screen_util.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart';
 
 
 ///必须在首页进行初始化
