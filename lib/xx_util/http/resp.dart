fce5e62b2218259401e3e3d0c415eb7dec47e8b7
kk <758124607@qq.com>
2022-10-19 18:04:49 +0800

    增加http_util

---
+++ lib/xx_util/http/resp.dart
@@ -0,0 +1,31 @@
+/// 返回数据
+class Resp {
+  String? code;
+  String? errorCode;
+  dynamic msg;
+  dynamic data;
+  dynamic errorDesc;
+
+  Resp({this.code, this.msg});
+
+  Resp.fromJson(Map<String, dynamic> json) {
+    code = json['code'];
+    errorCode = json['errorcode'];
+    msg = json['msg'] ?? '';
+    errorDesc = json['errordesc'] ?? '';
+    data = json['data'] ;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['code'] = code;
+    data['errorcode'] = errorCode;
+    data['msg'] = msg;
+    data['errordesc'] = errorDesc;
+    data['data'] = this.data;
+    return data;
+  }
+
+  /// 是否成功
+  bool get success => code ==  "0";
+}
