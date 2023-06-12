9b1bbfc6b7ddd6de018989a26c7187ea050208e9
kk <758124607@qq.com>
2023-06-12 14:30:24 +0800

    删除屏幕适配

--- lib/xx_util/screen/screen_util.dart
+++ lib/xx_util/screen/screen_util.dart
@@ -1,51 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-export 'package:flutter_screenutil/flutter_screenutil.dart';
 
-
-///必须在首页进行初始化
-initScreenUtil({
-  required double width,
-  required double height,
-  required BuildContext context,
-}) {
-  ScreenUtil.init(
-    //设备像素大小
-    context,
-    //设计尺寸
-    designSize: Size(width, height),
-  );
-}
+import '../../xx_ui/base/base.dart';
 
 ///按照屏幕宽缩放
 double scale(num width) {
-  return ScreenUtil().setWidth(width);
-}
-
-///按照屏幕高缩放
-double scaleByHeight(num height) {
-  return ScreenUtil().setHeight(height);
+  return width.toDouble();
 }
 
 ///字体适配
 double font(double fontSize) {
-  return fontSize.sp;
+  return fontSize;
 }
 
 ///屏幕宽度
 double getScreenWidth() {
-  return ScreenUtil().screenWidth;
+  return MediaQuery.of(Get.context!).size.width;
 }
 
 ///屏幕高度
 double getScreenHeight() {
-  return ScreenUtil().screenHeight;
+  return MediaQuery.of(Get.context!).size.height;
 }
-
-///屏幕密度
-double getScreenPixelRatio() {
-  return ScreenUtil().pixelRatio ?? 1;
-}
-
-
