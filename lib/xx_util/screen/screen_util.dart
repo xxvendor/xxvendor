0d0b31570b6ca4e105f3b64096b16902bb13965f
kk <758124607@qq.com>
2023-07-21 15:51:19 +0800

    增加屏幕适配

--- lib/xx_util/screen/screen_util.dart
+++ lib/xx_util/screen/screen_util.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../xx_ui/base/base.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
+initScreenUtil({
+  required double width,
+  required double height,
+  required BuildContext context,
+}) {
+  ScreenUtil.init(
+    //设备像素大小
+    context,
+    //设计尺寸
+    designSize: Size(width, height),
+  );
+}
 
 ///按照屏幕宽缩放
 double scale(num width) {
-  return width.toDouble();
+  return ScreenUtil().setWidth(width);
 }
 
 ///字体适配
-double font(double fontSize) {
-  return fontSize;
+double font(num fontSize) {
+  return ScreenUtil().setSp(fontSize);
 }
 
 ///屏幕宽度
